3b1dc88847a40031c81c7a3cbf1110eefdb55dc9
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2021-12-16 07:56:22 +0100

    Add project files.

---
+++ Proefwerk_TSO_Deel1_Opdracht1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proefwerk_TSO_Deel1_Opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
